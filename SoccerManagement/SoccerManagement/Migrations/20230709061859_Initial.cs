ebe8bc8bd4de03909858bdfdaa21be6dd3c89c4f
FeehAvelar <felipealpaca@gmail.com>
2023-07-09 03:39:45 -0300

    Add Migration

---
+++ SoccerManagement/SoccerManagement/Migrations/20230709061859_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoccerManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Surname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cellphone = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateAdd = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateUpdate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    WhoChange = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
--- SoccerManagement/SoccerManagement/Models/Enities/Player.cs
+++ SoccerManagement/SoccerManagement/Models/Enities/Player.cs
@@ -1,19 +1,21 @@
 ﻿using SoccerManagement.Models.Enities.BasesEntities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SoccerManagement.Models.Enities
 {
+    [Table("Player")]
     public sealed class Player : EntityWithDates<int>
     {
         public string Name { get; set; }        
         public string Surname { get; set; } //Apelido
         public string Cellphone { get; set; }
         public string Email { get; set; }
-        public UsuarioEntity Usuario { get; set; }
+        //public UsuarioEntity Usuario { get; set; }
     }
 
-    public class UsuarioEntity : EntityWithOwner<int>
-    {
-        public string Username { get; set; }
-        public string Passworld { get; set; }
-    }
+    //public class UsuarioEntity : EntityWithOwner<int>
+    //{
+    //    public string Username { get; set; }
+    //    public string Passworld { get; set; }
+    //}
 }
